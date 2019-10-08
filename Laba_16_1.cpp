1f35ca381648ba19f1afbb411d8565547e2fcd06
KsushaRomano <55737387+KsushaRomano@users.noreply.github.com>
2019-10-09 00:11:35 +0300

    Add files via upload

---
+++ Laba_16_1.cpp
@@ -0,0 +1,30 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N, i, k, n;
+	k = 0;
+	n = 0;
+	cout << "Введите размер массива: ";
+	cin >> N;
+	int* A = new int[N];
+	cout << "Введите элементы массива: " << endl;
+	for (i = 0; i < N; i++) {
+		cin >> A[i];
+	};
+	int* B = new int[N];
+	for (i = 0; i < N; i++) {
+		if (A[i] != A[i - 1]) {
+			B[k] = A[i];
+			k++;
+			n++;
+		};
+	};
+	cout << "Измененный массив: " << endl;
+	for (k = 0; k < n; k++) {
+		cout << B[k] << endl;
+	};
+}
---
+++ Laba_16_2.cpp
@@ -0,0 +1,37 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N, i, k, n, j, m;
+	k = 1;
+	n = 0;
+	m = 0;
+	cout << "Введите размер массива: ";
+	cin >> N;
+	int* A = new int[N];
+	cout << "Введите элементы массива: " << endl;
+	for (i = 0; i < N; i++) {
+		cin >> A[i];
+	};
+	int* B = new int[N];
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			if ((i != j) & (A[i] == A[j])) {
+				k++;
+			};
+		};
+		if (k != 2) {
+			B[m] = A[i];
+			n++;
+			m++;
+		};
+		k = 1;
+	};
+	cout << "Измененный массив: " << endl;
+	for (m = 0; m < n; m++) {
+		cout << B[m] << endl;
+	};
+}
